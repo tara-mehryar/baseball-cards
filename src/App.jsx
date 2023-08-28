d20e4df2c8461851f59a4a9046ce99758ce469ad
Tara Mehryar <tara.mehryar@gmail.com>
2023-08-28 16:05:06 -0500

    Lab

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,46 @@
+import playerData from "./playerData.js";
+import { useState } from "react";
+
 function BaseballCard(props) {
+  let [showPicture, setShowPicture] = useState(true);
+
+  const toggleCard = () => {
+    setShowPicture(!showPicture)
+  };
+
+  if (showPicture){
   return (
-    <div className="card">
-      <h2>Player Name goes here</h2>
+    <div className="card" onClick={ toggleCard }>
+      <h2>{props.name}</h2>
+      <img src={props.imgUrl} />
+    </div>
+  )} else {
+  const statsDisplay = Object.entries(props.stats).map(([statName, statValue]) => (
+    <p name={statName}> {statName}:{statValue} </p>
+    ));
+  return (
+    <div className="card" onClick={ toggleCard }>
+      <h2> {props.name} </h2>
+      <p> Team: {props.team} </p>
+      <p> Position: {props.position} </p>
+      {statsDisplay}
     </div>
   );
-}
+}}
 
 function App() {
-  return <BaseballCard />;
+  const cards = playerData.map((player) => (
+  <BaseballCard 
+  name={player.name}
+  team={player.team}
+  position={player.position}
+  stats={player.stats}
+  imgUrl={player.imgUrl}
+  key={player.cardId}
+  />
+  ));
+
+  return <>{cards}</>;
 }
 
 export default App;
